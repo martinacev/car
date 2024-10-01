da3db8df630d873fc0f767f59fdf04ce41979b1b
Martin <martin_acevski@outlook.com>
2024-10-01 15:08:25 +0200

    add button

--- car-dealer/src/components/hero/Hero.jsx
+++ car-dealer/src/components/hero/Hero.jsx
@@ -2,12 +2,18 @@ import PropTypes from 'prop-types';
 import classes from './Hero.module.css';
 import logo from '/hero-car.webp';
 
+import Button from '../cta-button/cta-button';
+
 const Hero = ({ title, subtitle }) => {
     return (
         <div className={classes.container}>
             <div className={classes.content}>
                 <h2 className={classes.title}>{title}</h2>
                 <p className={classes.subtitle}>{subtitle}</p>
+                <div className={classes.wrapper}>
+                 <Button label="Contact Us" size='medium' styleType="danger"></Button>
+                 <Button label="Test Drive" size='medium' styleType="danger"></Button>
+                </div>
             </div>
             <div className={classes.image}>
                <img className={classes.logo} src={logo} alt='car' />
