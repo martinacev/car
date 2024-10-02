d0127018df391be73654bcce0848d1874ba4c0c4
Martin <martin_acevski@outlook.com>
2024-10-02 08:43:38 +0200

    add main section

--- car-dealer/src/components/main/Main.jsx
+++ car-dealer/src/components/main/Main.jsx
@@ -1,9 +1,8 @@
 import classes from './main.module.css';  
 import PropTypes from 'prop-types';
 
-import wheel from '../../assets/svg/wheel.svg';
-import statistic from '../../assets/svg/wheel.svg';
-import document from '../../assets/svg/wheel.svg';
+
+
 
 
 const Main = ({title, subtitle}) => {
@@ -14,23 +13,21 @@ const Main = ({title, subtitle}) => {
             <p className={classes.subtitle}>{subtitle}</p>
             <p className={classes.border}></p>
         </div>
-        <div className={classes.section}>
-            <div className={classes.logo}>
-              <img className={classes.svg} src={wheel} alt='logo' />
-            </div>
-        </div>
+        
      </div>   
     )
 }
 
 Main.defaultProps = {
     title: "Why Us?",
-    subtitle: "Because we know what is best for you"
+    subtitle: "Because we know what is best for you",
+    
 }
 
 Main.propTypes = {
     title: PropTypes.string,
-    subtitle: PropTypes.string
+    subtitle: PropTypes.string,
+   
 }
 
 export default Main;
